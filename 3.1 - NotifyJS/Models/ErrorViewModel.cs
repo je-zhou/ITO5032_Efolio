b0031c73cca5296a280960885818ab13807f7b8e
Jerry Zhou <81694589+je-zhou@users.noreply.github.com>
2024-07-24 16:16:52 +1000

    3.1

---
+++ 3.1 - NotifyJS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _3._1___NotifyJS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
